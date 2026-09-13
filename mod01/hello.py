ededa3c1f6b2f1ad3f30c63fb0bdbccedd043388
Jussi Saarela <jussiksa@metropolia.fi>
2026-09-13 14:20:19 +0300

    ensimmäinen lisäys: moduulit ja harjoitukset

---
+++ mod01/hello.py
@@ -0,0 +1,11 @@
+print ("hei maailma")
+print ("mikä sinun nimesi on?")
+name=input ("Jussi Saarela" )
+print ("terve , Jussi Saarela")
+# Ohjelmisto 1 - Python harjoitukset
+#**Jussi Saarela**
+# ## Moduuli 1
+#Tein tehtävät 1 ja 2.
+
+
+
